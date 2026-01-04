155628bb99b0d418581f9adbc3aa2029b448a0a6
Krishna Gupta <krishnagupta@Krishnas-MacBook-Air.local>
2026-01-04 13:28:35 +0530

    code added

--- SearchingAndSorting/BubbleSort/bubbleSort.js
+++ SearchingAndSorting/BubbleSort/bubbleSort.js
@@ -21,4 +21,6 @@ function bubbleSort(arr) {
       }
     }
   }
+
+  return arr; 
 }
---
+++ SearchingAndSorting/SelectionSort/selectionSort.js
@@ -0,0 +1,21 @@
+//Selection Sort is a comparison-based sorting algorithm. It sorts by repeatedly selecting the smallest (or largest) element from the unsorted portion and swapping it with the first unsorted element.
+
+function selectionSort(arr) {
+  //array lenght
+  let n = arr.length;
+  //iteration
+  //In each iteration it find the min element and put in the starting index
+  for (let i = 0; i < n - 1; i++) {
+    //second iteration finding the minimum value and replacing with 0th position
+    //this loop is only for finding the min element/value
+    let min = i;
+    for (j = i + 1; j < n - 1; j++) {
+      if (arr[j] < arr[min]) {
+        //if the current value is min than replace/ swap
+        min = j;
+      }
+    }
+  }
+
+  return arr;
+}
